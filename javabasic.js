1e15d26d84eb88bde724eb98714fc4e3b239015e
gaikwadarun <118682233+gaikwadarun@users.noreply.github.com>
2023-01-12 23:26:57 +0530

    new file added

---
+++ javabasic.js
@@ -0,0 +1,15 @@
+// var x= 15;
+// x*=89900;
+// console.log(x)
+// function wordblank(myNoun, Myadjective, myVerb, myAdverb)
+// {
+//     var result ="";
+//     result += "The "+ Myadjective +" "+myNoun+" "+myVerb+ "  to the store"
+//     return result;
+
+// }
+// console.log(wordblank("Dog","big", "ran","quickly"));
+var myarray = ["arun",8881212, "gmail",12345];
+myarray.push["gaikwad", 3];
+console.log(myarray);
+
